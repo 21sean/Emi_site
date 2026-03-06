919f3d79f1b7e02dc09b445dcc2e5ba9680dd87e
Emi <emi@Emis-MacBook-Air.local>
2026-03-05 23:13:30 -0800

    Reorder specialties

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -87,11 +87,11 @@ const profile: Profile = {
     "Currently pursuing an MBA at the University of California, San Diego with a focus on STEM and recognized as a UCSD Rady Scholar Fellowship recipient. At Dassault Systèmes, contributing to global SaaS marketing efforts for their BIOVIA brand by developing and executing international campaigns, conducting cross-market analyses, and optimizing strategies to enhance engagement and ROI.",
   location: "🌴 San Diego, CA",
   specialties: [
-    "SaaS",
-    "Semiconductor Industry",
-    "Pharmaceutical & Biotech Industry",
     "Global B2B Marketing",
     "Strategy",
+    "Semiconductor Industry",
+    "Pharmaceutical & Biotech Industry",
+    "SaaS",
     "AI & Advanced Manufacturing",
   ],
   socialLinks: [
